98e01299ab07e5f662f1c2b9b0e1a8352efb1d64
mouad maataoui <mouadmaataoui9@gmail.com>
2023-06-27 10:31:24 +0200

    ajout de fonctionalité supression

--- View/Product/products.php
+++ View/Product/products.php
@@ -1,13 +1,14 @@
 <div class="products">
     <h2>Liste des produits</h2>
     <div class="products-list">
-        <?php foreach($products as $product): ?>
+        <?php foreach($products as $product): 
+            if($product->deletion != 1){?>
             <div class="product-card" data-id="<?= $product->id ?>">
                 <!-- <img src="<?= $product->images ?>" alt=""> -->
                 <h3><?= $product->name ?></h3>
                 <p class="desc"><?= $product->description ?></p>
                 <p class="price"><?= $product->price ?></p>
             </div>
-        <?php endforeach; ?>
+        <?php }endforeach; ?>
     </div>
 </div>
